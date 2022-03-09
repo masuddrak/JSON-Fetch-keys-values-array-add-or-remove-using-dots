f418d42984b33cad8f5151d128af94967fdd7878
masud rana <masudranaacb@gmail.com>
2022-03-09 07:49:25 -0800

    fetch

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const student={
     subject:'Mathematics',
     age:22
 }
+// conver object json file 
 const student1=JSON.stringify(student)
 // console.log(student1)
+
+// conver json to normal object
 const student2=JSON.parse(student1)
-console.log(student2)
+// console.log(student2)
+
+
+
+// fetch using
+fetch('url')
+.then(res=>res.json())
+.then(data=>console.log(data))
